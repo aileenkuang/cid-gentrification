433ba174280c1be841d984032a02d73d2e3fa730
aileenkuang <124929180+aileenkuang@users.noreply.github.com>
2023-12-01 12:13:29 -0800

    delete duplicate entries

--- data-cleaning.js
+++ data-cleaning.js
@@ -28,13 +28,36 @@ fs.readFile(builtUnitsFile, 'utf8', (err, data) => {
         return data
     }
 
-    function deleteDuplicateEntries(data) {
-        
+    // Function to delete entries that are duplicated
+    function deleteDuplicateEntries(data, addressProperty, yearProperty,
+        permitProperty) {
+        const uniqueFeatures = {};
+
+        data.features.forEach(feature => {
+            const address = feature.properties[addressProperty];
+            const year = feature.properties[yearProperty];
+            const permit = feature.properties[permitProperty]
+            const key = `${address}-${year}-${permit}`;
+
+            if (!uniqueFeatures[key]) {
+                uniqueFeatures[key] = feature;
+            } else {
+                return;
+            }
+        });
+
+        const uniqueFeaturesArray = Object.values(uniqueFeatures);
+
+        data.features = uniqueFeaturesArray;
+
+        return data;
     }
 
     const cleanedBuiltUnitsData = deleteUnecessaryProperties(builtUnitsData)
+    const dataWithoutDuplicates = deleteDuplicateEntries(cleanedBuiltUnitsData, 
+        "ADDRESS", "YEAR", "APTYPE")
 
-    const cleanedDataString = JSON.stringify(cleanedBuiltUnitsData, null, 2);
+    const cleanedDataString = JSON.stringify(dataWithoutDuplicates, null, 2);
     
     fs.writeFile('assets/built-units-since-2010.geojson', cleanedDataString, (writeErr) => {
         if (writeErr) {
@@ -43,5 +66,21 @@ fs.readFile(builtUnitsFile, 'utf8', (err, data) => {
             console.log('GeoJSON file cleaned and saved successfully.');
         }
     });
+
 })
 
+const cleanedFile = 'assets/built-units-since-2010.geojson';
+
+fs.readFile(cleanedFile, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading GeoJSON file:', err);
+        return;
+    }
+
+    const cleanedData = JSON.parse(data);
+
+    // Get the number of features
+    const numberOfFeatures = cleanedData.features.length;
+
+    console.log(`Number of features in the GeoJSON: ${numberOfFeatures}`);
+});
